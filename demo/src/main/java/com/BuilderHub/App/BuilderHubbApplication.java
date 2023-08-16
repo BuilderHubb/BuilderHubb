62ac2cd224f4c925f8636ee98e250857f0a93d2c
robertas miskinis <robertukas.2a@gmail.com>
2023-08-16 21:42:46 +0300

    Start

---
+++ demo/src/main/java/com/BuilderHub/App/BuilderHubbApplication.java
@@ -0,0 +1,13 @@
+package com.BuilderHub.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderHubbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuilderHubbApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/BuilderHub/App/BuilderHubbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BuilderHub.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderHubbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
